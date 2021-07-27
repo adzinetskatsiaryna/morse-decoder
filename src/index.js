850f14b90e0a51af0614d1f90ebf814d064daffd
adzinets <adzinetskatsiaryna@gmail.com>
2021-07-27 18:26:44 +0300

    add the solution of the task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+   // перебераем строку с шагом i=10 на каждой иттерации
+    for(let i = 0; i<expr.length; i+=10){
+        arr.push(expr.substr(i, 10)) //пушим в массив подстроки от позиции i размером 10
+       // console.log(arr)
+    }
+   
+    for (let item of arr) {
+      let morse = item.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-'); // заменяем 00, 10, 11 и * на '.' и '-'
+      arr.splice(arr.indexOf(item),1, MORSE_TABLE[morse] || ' ') //удаляем 1 элмент в массиве по индексу и вставляем букву на его место из обекта MORSE_TABLE, если не нашлось совпадение вставляем пробел
+    }
+    return arr.join('')
+    
 }
 
 module.exports = {
     decode
 }
+
+
+
